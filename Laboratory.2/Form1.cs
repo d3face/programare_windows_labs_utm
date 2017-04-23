ea4bcf3de17baa46561e434ce1bfdee18973a6b7
User <User@DESKTOP-33BH994>
2017-04-23 15:13:27 +0300

    Added Laboratory 2

---
+++ Laboratory.2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratory._2
+{
+    public partial class Form1 : Form
+    {
+        private const int V = 80;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //base.OnPaint(e);
+            // Create pen.
+            Pen blackPen = new Pen(Color.Black, 2);
+
+            // Create points that define line.
+            Point point1 = new Point(100, 100);
+            Point point2 = new Point(500, 100);
+            int[] px = new int[100];
+            int[] py = new int[100];
+            for (int i = 0; i < V; i++)
+            {
+                px[i] = 50 + (i % 20) * 20;
+                py[i] = 50 + ((i + 15) % 30) * 20;
+            }
+            for (int i = 1; i < V; i++)
+            {
+                e.Graphics.DrawLine(blackPen, px[i - 1], py[i - 1], px[i], py[i]);
+            }
+                        
+            e.Graphics.DrawEllipse(blackPen, 500, 100, 700, 200);
+        }
+    }
+}
---
+++ Laboratory.2/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Laboratory.2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Laboratory.2")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("de9e4cd4-bcfd-4be2-bf44-27544fd14fbd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: PrimaryInteropAssembly(4, 2)]
