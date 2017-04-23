22e9cc846cc08f09f3360778eb2cb89dedc9e0dd
User <User@DESKTOP-33BH994>
2017-04-23 13:46:23 +0300

    Added Laboratory.Main

---
+++ Laboratory.Main/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Laboratory.Main
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.start_lab1 = new System.Windows.Forms.Button();
+            this.start_lab2 = new System.Windows.Forms.Button();
+            this.start_lab3 = new System.Windows.Forms.Button();
+            this.start_lab4 = new System.Windows.Forms.Button();
+            this.start_lab5 = new System.Windows.Forms.Button();
+            this.start_lab6 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.start_lab1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.start_lab2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.start_lab3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.start_lab4, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.start_lab5, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.start_lab6, 1, 5);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(970, 317);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // start_lab1
+            // 
+            this.start_lab1.Location = new System.Drawing.Point(488, 3);
+            this.start_lab1.Name = "start_lab1";
+            this.start_lab1.Size = new System.Drawing.Size(170, 23);
+            this.start_lab1.TabIndex = 0;
+            this.start_lab1.Text = "Start Laboratory.1";
+            this.start_lab1.UseVisualStyleBackColor = true;
+            // 
+            // start_lab2
+            // 
+            this.start_lab2.Location = new System.Drawing.Point(488, 55);
+            this.start_lab2.Name = "start_lab2";
+            this.start_lab2.Size = new System.Drawing.Size(170, 23);
+            this.start_lab2.TabIndex = 1;
+            this.start_lab2.Text = "Start Laboratory.2";
+            this.start_lab2.UseVisualStyleBackColor = true;
+            // 
+            // start_lab3
+            // 
+            this.start_lab3.Location = new System.Drawing.Point(488, 107);
+            this.start_lab3.Name = "start_lab3";
+            this.start_lab3.Size = new System.Drawing.Size(170, 23);
+            this.start_lab3.TabIndex = 2;
+            this.start_lab3.Text = "Start Laboratory.3";
+            this.start_lab3.UseVisualStyleBackColor = true;
+            // 
+            // start_lab4
+            // 
+            this.start_lab4.Location = new System.Drawing.Point(488, 159);
+            this.start_lab4.Name = "start_lab4";
+            this.start_lab4.Size = new System.Drawing.Size(170, 23);
+            this.start_lab4.TabIndex = 3;
+            this.start_lab4.Text = "Start Laboratory.4";
+            this.start_lab4.UseVisualStyleBackColor = true;
+            // 
+            // start_lab5
+            // 
+            this.start_lab5.Location = new System.Drawing.Point(488, 211);
+            this.start_lab5.Name = "start_lab5";
+            this.start_lab5.Size = new System.Drawing.Size(170, 23);
+            this.start_lab5.TabIndex = 4;
+            this.start_lab5.Text = "Start Laboratory.5";
+            this.start_lab5.UseVisualStyleBackColor = true;
+            // 
+            // start_lab6
+            // 
+            this.start_lab6.Location = new System.Drawing.Point(488, 263);
+            this.start_lab6.Name = "start_lab6";
+            this.start_lab6.Size = new System.Drawing.Size(170, 23);
+            this.start_lab6.TabIndex = 5;
+            this.start_lab6.Text = "Start Laboratory.6";
+            this.start_lab6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1033, 557);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button start_lab1;
+        private System.Windows.Forms.Button start_lab2;
+        private System.Windows.Forms.Button start_lab3;
+        private System.Windows.Forms.Button start_lab4;
+        private System.Windows.Forms.Button start_lab5;
+        private System.Windows.Forms.Button start_lab6;
+    }
+}
+
