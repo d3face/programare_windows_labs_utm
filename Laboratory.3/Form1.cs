1f6e9da0bc1f47d65214f9b30cd341b9062e185f
User <User@DESKTOP-33BH994>
2017-04-24 18:34:58 +0300

    Changed started points on Bezier spline (Laboratory.3)

--- Laboratory.3/Form1.cs
+++ Laboratory.3/Form1.cs
@@ -18,15 +18,15 @@ namespace Laboratory._3
         }
         private List<Point> points = new List<Point>
         {
-            new Point { X = 0, Y = 0 },
-            new Point { X = 100, Y = 100 },
-            new Point { X = 120, Y = 200 },
-            new Point { X = 140, Y = 250 },
+            new Point { X = 127, Y = 159 },
+            new Point { X = 237, Y = 122 },
+            new Point { X = 351, Y = 191 },
+            new Point { X = 277, Y = 316 },
 
-            new Point { X = 160, Y = 310 },
-            new Point { X = 150, Y = 350 },
-            new Point { X = 80, Y = 360 },
-        }.Select(x => Point.Add(x, new Size(200, 100))).ToList();
+            new Point { X = 211, Y = 431 },
+            new Point { X = 46, Y = 436 },
+            new Point { X = 201, Y = 293 },
+        }.Select(x => Point.Add(x, new Size(100, -70))).ToList();
         protected override void OnPaint(PaintEventArgs e)
         {
             e.Graphics.DrawBeziers(new Pen(Color.Black, 2), points.Select(x => x).ToArray());
