cca68265574099a8582b159a68ff665169cb80c9
User <User@DESKTOP-33BH994>
2017-04-24 18:15:52 +0300

    Added description for each Laboratory

--- Laboratory.Main/Form1.Designer.cs
+++ Laboratory.Main/Form1.Designer.cs
@@ -35,20 +35,32 @@
             this.start_lab4 = new System.Windows.Forms.Button();
             this.start_lab5 = new System.Windows.Forms.Button();
             this.start_lab6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 64.63918F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35.36082F));
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.start_lab1, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.start_lab2, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.start_lab3, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.start_lab4, 1, 3);
             this.tableLayoutPanel1.Controls.Add(this.start_lab5, 1, 4);
             this.tableLayoutPanel1.Controls.Add(this.start_lab6, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 6;
@@ -63,7 +75,7 @@
             // 
             // start_lab1
             // 
-            this.start_lab1.Location = new System.Drawing.Point(488, 3);
+            this.start_lab1.Location = new System.Drawing.Point(630, 3);
             this.start_lab1.Name = "start_lab1";
             this.start_lab1.Size = new System.Drawing.Size(170, 23);
             this.start_lab1.TabIndex = 0;
@@ -73,7 +85,7 @@
             // 
             // start_lab2
             // 
-            this.start_lab2.Location = new System.Drawing.Point(488, 55);
+            this.start_lab2.Location = new System.Drawing.Point(630, 55);
             this.start_lab2.Name = "start_lab2";
             this.start_lab2.Size = new System.Drawing.Size(170, 23);
             this.start_lab2.TabIndex = 1;
@@ -83,7 +95,7 @@
             // 
             // start_lab3
             // 
-            this.start_lab3.Location = new System.Drawing.Point(488, 107);
+            this.start_lab3.Location = new System.Drawing.Point(630, 107);
             this.start_lab3.Name = "start_lab3";
             this.start_lab3.Size = new System.Drawing.Size(170, 23);
             this.start_lab3.TabIndex = 2;
@@ -93,7 +105,7 @@
             // 
             // start_lab4
             // 
-            this.start_lab4.Location = new System.Drawing.Point(488, 159);
+            this.start_lab4.Location = new System.Drawing.Point(630, 159);
             this.start_lab4.Name = "start_lab4";
             this.start_lab4.Size = new System.Drawing.Size(170, 23);
             this.start_lab4.TabIndex = 3;
@@ -103,7 +115,7 @@
             // 
             // start_lab5
             // 
-            this.start_lab5.Location = new System.Drawing.Point(488, 211);
+            this.start_lab5.Location = new System.Drawing.Point(630, 211);
             this.start_lab5.Name = "start_lab5";
             this.start_lab5.Size = new System.Drawing.Size(170, 23);
             this.start_lab5.TabIndex = 4;
@@ -113,13 +125,73 @@
             // 
             // start_lab6
             // 
-            this.start_lab6.Location = new System.Drawing.Point(488, 263);
+            this.start_lab6.Location = new System.Drawing.Point(630, 263);
             this.start_lab6.Name = "start_lab6";
             this.start_lab6.Size = new System.Drawing.Size(170, 23);
             this.start_lab6.TabIndex = 5;
             this.start_lab6.Text = "Start Laboratory.6";
             this.start_lab6.UseVisualStyleBackColor = true;
             this.start_lab6.Click += new System.EventHandler(this.lab6);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Aplicatii Windows";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Interfaţa GDI/GDI+";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Curbe Bezier";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(340, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Principiile de lucru cu tastatura";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(522, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Principiile de lucru cu mouse-ul (bazat pe 3 si 4)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(3, 260);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(299, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Ceasul sistemului Windows";
             // 
             // Form1
             // 
@@ -130,6 +202,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -143,6 +216,12 @@
         private System.Windows.Forms.Button start_lab4;
         private System.Windows.Forms.Button start_lab5;
         private System.Windows.Forms.Button start_lab6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
